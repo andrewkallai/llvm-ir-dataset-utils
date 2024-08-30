a585725df731ee6ee838f8b6e8c44bc83a39df81
Andrew Kallai <kallai.andrew@gmail.com>
2024-08-30 13:20:32 -0400

    Fixed code formatting, modified docstrings, and improved outlier analysis process in read_column.py.

--- llvm_ir_dataset_utils/compile_time_analysis_tools/python_processing/fitting_and_plotting.py
+++ llvm_ir_dataset_utils/compile_time_analysis_tools/python_processing/fitting_and_plotting.py
@@ -1,4 +1,4 @@
-"""Utilies for plotting instruction counts & text segment size scatter plots, as well as instruction counts histograms.
+"""plot_instruction_counts_v_textseg is used to make compiler instruction counts & text segment size scatter plots, and plot_instruction_counts_histograms is used to make compiler instruction counts histograms.
 
 plot_instruction_counts_v_textseg
   Returns: None
@@ -12,55 +12,61 @@ import matplotlib.pyplot as plt
 from read_column import csv_to_pandas_df
 
 
-def plot_instruction_counts_v_textseg(lang: str, storage: str, show: bool = False) -> None:
-    import numpy as np
-    df = csv_to_pandas_df(lang, storage)
-    textseg_data = df["text_segment"]
-    inst_data = df["instruction"]
-    c, b, a = np.polyfit(textseg_data, inst_data, 2)
+def plot_instruction_counts_v_textseg(lang: str,
+                                      storage: str,
+                                      show: bool = False) -> None:
+  import numpy as np
+  df = csv_to_pandas_df(lang, storage)
+  textseg_data = df["text_segment"]
+  inst_data = df["instruction"]
+  c, b, a = np.polyfit(textseg_data, inst_data, 2)
 
-    x_axis = range(min(textseg_data), max(textseg_data), 10)
-    z = np.polyval([c, b, a], x_axis)
+  x_axis = range(min(textseg_data), max(textseg_data), 10)
+  z = np.polyval([c, b, a], x_axis)
 
-    plt.scatter(textseg_data, inst_data)
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.gca().set_ylim([10**8, 10**13])
-    plt.gca().set_xlim([10**(-1), 10**9])
-    plt.xlabel("Text Segment Size (bytes)")
-    plt.ylabel("Compiler CPU Instructions Count")
-    if (lang == "cpp"):
-        plt.title("Clang++ Compiler Instructions vs. Text Segment Size ("+lang+")")
-    else:
-        plt.title("Clang Compiler Instructions vs. Text Segment Size ("+lang+")")
-    plt.plot(x_axis, z, 'r')
-    equation = f"${c:.1e}x^2 + {b:.1e}x + {a:.1e}$"
-    plt.legend([f"fit: {equation}", "original"])
-    if (show):
-        print(len(textseg_data))
-        plt.show()
-    else:
-        plt.savefig(fname=lang+"_instvtext.pdf", format="pdf")
-    plt.close()
+  plt.scatter(textseg_data, inst_data)
+  plt.xscale("log")
+  plt.yscale("log")
+  plt.gca().set_ylim([10**8, 10**13])
+  plt.gca().set_xlim([10**(-1), 10**9])
+  plt.xlabel("Text Segment Size (bytes)")
+  plt.ylabel("Compiler CPU Instructions Count")
+  if (lang == "cpp"):
+    plt.title("Clang++ Compiler Instructions vs. Text Segment Size (" + lang +
+              ")")
+  else:
+    plt.title("Clang Compiler Instructions vs. Text Segment Size (" + lang +
+              ")")
+  plt.plot(x_axis, z, 'r')
+  equation = f"${c:.1e}x^2 + {b:.1e}x + {a:.1e}$"
+  plt.legend([f"fit: {equation}", "original"])
+  if (show):
+    print(len(textseg_data))
+    plt.show()
+  else:
+    plt.savefig(fname=lang + "_instvtext.pdf", format="pdf")
+  plt.close()
 
 
-def plot_instruction_counts_histograms(lang: str, storage: str, show: bool = False) -> None:
-    df = csv_to_pandas_df(lang, storage)
-    inst_data = df["instruction"]
-    plt.hist(inst_data, bins='auto', alpha=1, color='b')
-    plt.title("Histogram of Compiler Instructions ("+lang+")")
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.gca().set_ylim([10**(-1), 10**5])
-    plt.gca().set_xlim([10**8, 10**13])
-    plt.xlabel('Compiler CPU Instructions Count')
-    plt.ylabel('No. of IR Files')
+def plot_instruction_counts_histograms(lang: str,
+                                       storage: str,
+                                       show: bool = False) -> None:
+  df = csv_to_pandas_df(lang, storage)
+  inst_data = df["instruction"]
+  plt.hist(inst_data, bins='auto', alpha=1, color='b')
+  plt.title("Histogram of Compiler Instructions (" + lang + ")")
+  plt.xscale("log")
+  plt.yscale("log")
+  plt.gca().set_ylim([10**(-1), 10**5])
+  plt.gca().set_xlim([10**8, 10**13])
+  plt.xlabel('Compiler CPU Instructions Count')
+  plt.ylabel('No. of IR Files')
 
-    plt.text(max(inst_data), 1.1, format(max(inst_data), '.2e'), ha='center')
+  plt.text(max(inst_data), 1.1, format(max(inst_data), '.2e'), ha='center')
 
-    if (show):
-        print(len(inst_data))
-        plt.show()
-    else:
-        plt.savefig(fname=lang+"_hist.pdf", format="pdf")
-    plt.close()
+  if (show):
+    print(len(inst_data))
+    plt.show()
+  else:
+    plt.savefig(fname=lang + "_hist.pdf", format="pdf")
+  plt.close()
--- llvm_ir_dataset_utils/compile_time_analysis_tools/python_processing/read_column.py
+++ llvm_ir_dataset_utils/compile_time_analysis_tools/python_processing/read_column.py
@@ -1,4 +1,4 @@
-"""Utilities for reading and processing csv data with features for outlier analysis.
+"""csv_to_pandas_df is used to read a IR features CSV file and create a Pandas dataframe for it, and outlier_rows is used to filter the Pandas dataframe to the IR files which can be classified as outliers. 
 
 csv_to_pandas_df
   Returns: pandas.core.frame.DataFrame
@@ -9,16 +9,25 @@ outlier_rows
 """
 
 
-def csv_to_pandas_df(lang: str, storage: str, file_name_suffix: str = 'combined.csv', write_to_csv: bool = False):
-    import pandas as pd
+def csv_to_pandas_df(lang: str,
+                     storage: str,
+                     file_name_suffix: str = 'combined.csv'):
+  import pandas as pd
 
-    df = pd.read_csv(storage+lang+'_'+file_name_suffix, skipinitialspace=True)
-    if (write_to_csv):
-        df.to_csv(lang+'_normalized_'+file_name_suffix, index=False)
-    return df
+  df = pd.read_csv(
+      storage + lang + '_' + file_name_suffix, skipinitialspace=True)
+  return df
 
 
-def outlier_rows(lang: str, storage: str):
-    df = csv_to_pandas_df(lang, storage)
-    outl = df.nlargest(df.shape[0]//2, "percentage")
-    return outl.nlargest(10, "instruction")
+def outlier_rows(lang: str,
+                 storage: str,
+                 outlier_num: int = 10,
+                 write_to_csv: bool = False):
+  df = csv_to_pandas_df(lang, storage)
+  outl = df.nlargest(df.shape[0] // 2, "percentage")
+  outl = outl[outl.instruction > outl["instruction"].quantile(
+      q=.75, interpolation='lower')]
+  outl = outl.nlargest(outlier_num, "percentage")
+  if (write_to_csv):
+    df.to_csv(lang + '_outliers.csv', index=False)
+  return outl
